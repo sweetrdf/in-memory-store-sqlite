20b449ce1e39d32aa6b61ba26ff371d5b048f9f0
Konrad Abicht <hi@inspirito.de>
2021-03-11 11:47:18 +0100

    removed obsolete functions from ARC2.php: getFormat, getPreferedFormat, getReader

--- ARC2.php
+++ ARC2.php
@@ -20,114 +20,6 @@ class ARC2
         return preg_match("/^\s*".$re.'(.*)$/'.$options, $v, $m) ? $m : false;
     }
 
-    /**
-     * @todo remove
-     */
-    public static function getFormat($v, $mtype = '', $ext = '')
-    {
-        $r = false;
-        /* mtype check (atom, rdf/xml, turtle, n3, mp3, jpg) */
-        $r = (!$r && preg_match('/\/atom\+xml/', $mtype)) ? 'atom' : $r;
-        $r = (!$r && preg_match('/\/rdf\+xml/', $mtype)) ? 'rdfxml' : $r;
-        $r = (!$r && preg_match('/\/(x\-)?turtle/', $mtype)) ? 'turtle' : $r;
-        $r = (!$r && preg_match('/\/rdf\+n3/', $mtype)) ? 'n3' : $r;
-        $r = (!$r && preg_match('/\/sparql-results\+xml/', $mtype)) ? 'sparqlxml' : $r;
-        /* xml sniffing */
-        if (
-            !$r &&
-            /* starts with angle brackets */
-            preg_match('/^\s*\<[^\s]/s', $v) &&
-            /* has an xmlns:* declaration or a matching pair of tags */
-            (preg_match('/\sxmlns\:?/', $v) || preg_match('/\<([^\s]+).+\<\/\\1\>/s', $v)) // &&
-        ) {
-            while (preg_match('/^\s*\<\?xml[^\r\n]+\?\>\s*/s', $v)) {
-                $v = preg_replace('/^\s*\<\?xml[^\r\n]+\?\>\s*/s', '', $v);
-            }
-            while (preg_match('/^\s*\<\!--.+?--\>\s*/s', $v)) {
-                $v = preg_replace('/^\s*\<\!--.+?--\>\s*/s', '', $v);
-            }
-            /* doctype checks (html, rdf) */
-            $r = (!$r && preg_match('/^\s*\<\!DOCTYPE\s+html[\s|\>]/is', $v)) ? 'html' : $r;
-            $r = (!$r && preg_match('/^\s*\<\!DOCTYPE\s+[a-z0-9\_\-]\:RDF\s/is', $v)) ? 'rdfxml' : $r;
-            /* markup checks */
-            $v = preg_replace('/^\s*\<\!DOCTYPE\s.*\]\>/is', '', $v);
-            $r = (!$r && preg_match('/^\s*\<rss\s+[^\>]*version/s', $v)) ? 'rss' : $r;
-            $r = (!$r && preg_match('/^\s*\<feed\s+[^\>]+http\:\/\/www\.w3\.org\/2005\/Atom/s', $v)) ? 'atom' : $r;
-            $r = (!$r && preg_match('/^\s*\<opml\s/s', $v)) ? 'opml' : $r;
-            $r = (!$r && preg_match('/^\s*\<html[\s|\>]/is', $v)) ? 'html' : $r;
-            $r = (!$r && preg_match('/^\s*\<sparql\s+[^\>]+http\:\/\/www\.w3\.org\/2005\/sparql\-results\#/s', $v)) ? 'sparqlxml' : $r;
-            $r = (!$r && preg_match('/^\s*\<[^\>]+http\:\/\/www\.w3\.org\/2005\/sparql\-results#/s', $v)) ? 'srx' : $r;
-            $r = (!$r && preg_match('/^\s*\<[^\s]*RDF[\s\>]/s', $v)) ? 'rdfxml' : $r;
-            $r = (!$r && preg_match('/^\s*\<[^\>]+http\:\/\/www\.w3\.org\/1999\/02\/22\-rdf/s', $v)) ? 'rdfxml' : $r;
-
-            $r = !$r ? 'xml' : $r;
-        }
-        /* json|jsonp */
-        if (!$r && preg_match('/^[a-z0-9\.\(]*\s*[\{\[].*/s', trim($v))) {
-            /* google social graph api */
-            $r = (!$r && preg_match('/\"canonical_mapping\"/', $v)) ? 'sgajson' : $r;
-            /* crunchbase api */
-            $r = (!$r && preg_match('/\"permalink\"/', $v)) ? 'cbjson' : $r;
-
-            $r = !$r ? 'json' : $r;
-        }
-        /* turtle/n3 */
-        $r = (!$r && preg_match('/\@(prefix|base)/i', $v)) ? 'turtle' : $r;
-        $r = (!$r && preg_match('/^(ttl)$/', $ext)) ? 'turtle' : $r;
-        $r = (!$r && preg_match('/^(n3)$/', $ext)) ? 'n3' : $r;
-        /* ntriples */
-        $r = (!$r && preg_match('/^\s*(_:|<).+?\s+<[^>]+?>\s+\S.+?\s*\.\s*$/sm', $v)) ? 'ntriples' : $r;
-        $r = (!$r && preg_match('/^(nt)$/', $ext)) ? 'ntriples' : $r;
-
-        return $r;
-    }
-
-    /**
-     * @todo remove
-     */
-    public static function getPreferredFormat($default = 'plain')
-    {
-        $formats = [
-            'html' => 'HTML', 'text/html' => 'HTML', 'xhtml+xml' => 'HTML',
-            'rdfxml' => 'RDFXML', 'rdf+xml' => 'RDFXML',
-            'ntriples' => 'NTriples',
-            'rdf+n3' => 'Turtle', 'x-turtle' => 'Turtle', 'turtle' => 'Turtle', 'text/turtle' => 'Turtle',
-            'rdfjson' => 'RDFJSON', 'json' => 'RDFJSON',
-            'xml' => 'XML',
-            'legacyjson' => 'LegacyJSON',
-        ];
-        $prefs = [];
-        $o_vals = [];
-        /* accept header */
-        $vals = explode(',', $_SERVER['HTTP_ACCEPT']);
-        if ($vals) {
-            foreach ($vals as $val) {
-                if (preg_match('/(rdf\+n3|(x\-|text\/)turtle|rdf\+xml|text\/html|xhtml\+xml|xml|json)/', $val, $m)) {
-                    $o_vals[$m[1]] = 1;
-                    if (preg_match('/\;q\=([0-9\.]+)/', $val, $sub_m)) {
-                        $o_vals[$m[1]] = 1 * $sub_m[1];
-                    }
-                }
-            }
-        }
-        /* arg */
-        if (isset($_GET['format'])) {
-            $o_vals[$_GET['format']] = 1.1;
-        }
-        /* rank */
-        arsort($o_vals);
-        foreach ($o_vals as $val => $prio) {
-            $prefs[] = $val;
-        }
-        /* default */
-        $prefs[] = $default;
-        foreach ($prefs as $pref) {
-            if (isset($formats[$pref])) {
-                return $formats[$pref];
-            }
-        }
-    }
-
     public static function splitURI($v)
     {
         /* the following namespaces may lead to conflated URIs,
@@ -230,13 +122,6 @@ class ARC2
         return new $cls($a, $caller);
     }
 
-    /* reader */
-
-    public static function getReader($a = '')
-    {
-        return self::getComponent('Reader', $a);
-    }
-
     /* parsers */
 
     public static function getParser($prefix, $a = '')
@@ -244,6 +129,9 @@ class ARC2
         return self::getComponent($prefix.'Parser', $a);
     }
 
+    /**
+     * @todo only for test purposes; use hardf instead and remove this
+     */
     public static function getTurtleParser($a = '')
     {
         return self::getParser('Turtle', $a);
